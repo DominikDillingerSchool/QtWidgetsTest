dc3769513f437d1cdfa7241d13dd18e542c5673c
DominikDillinger <dominik.dillinger@googlemail.com>
2024-02-24 21:00:08 +0100

    REF Tidy empty executable

--- emptyExecutable/mainwindow.cpp
+++ emptyExecutable/mainwindow.cpp
@@ -4,6 +4,4 @@
 MainWindow::MainWindow(QMainWindow *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    // connect(ui->BTN_storeA, &QPushButton::clicked, this, [&](bool) { m_samplesA = ui->processView->GetSamples(0); });
 }
